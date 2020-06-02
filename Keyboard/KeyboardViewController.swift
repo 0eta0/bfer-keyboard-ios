c27b0afa0d30f6154926d3ef967f921bf925659b
0eta0 <eta.develop@gmail.com>
2020-06-02 20:34:05 +0900

    impl input text

--- Keyboard/KeyboardViewController.swift
+++ Keyboard/KeyboardViewController.swift
@@ -10,15 +10,20 @@ import UIKit
 
 class KeyboardViewController: UIInputViewController {
 
+    // MARK: IBOutlet
+
     @IBOutlet var nextKeyboardButton: UIButton!
+
+    // MARK: View
+
     private var keyboardView: KeyboardLayoutView!
-//    private lazy var keyboardView: KeyboardLayoutView = {
-//
-//        return UINib(nibName: "KeyboardLayoutView", bundle: nil)
-//            .instantiate(withOwner: nil, options: nil)
-//            .first as! KeyboardLayoutView
-//    }()
     
+    // MARK: Properties
+
+    var timer: Timer?
+    
+    // MARK: Override
+
     override func updateViewConstraints() {
         super.updateViewConstraints()
         
@@ -30,15 +35,15 @@ class KeyboardViewController: UIInputViewController {
         
         // Perform custom UI setup here
         self.nextKeyboardButton = UIButton(type: .system)
-        
-        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard",
+                                                           comment: "Title for 'Next Keyboard' button"),
+                                         for: [])
         self.nextKeyboardButton.sizeToFit()
         self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
-        
         self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
         
         self.view.addSubview(self.nextKeyboardButton)
-        
+
         self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
         self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
         
@@ -57,6 +62,98 @@ class KeyboardViewController: UIInputViewController {
           keyboardView.rightAnchor.constraint(equalTo: inputView.rightAnchor),
           keyboardView.bottomAnchor.constraint(equalTo: inputView.bottomAnchor)
         ])
+        
+        /// [
+        self.keyboardView.button1.addTarget(self, action: #selector(button1Handler), for: .touchUpInside)
+        /// +
+        self.keyboardView.button2.addTarget(self, action: #selector(button2Handler), for: .touchUpInside)
+        /// -
+        self.keyboardView.button3.addTarget(self, action: #selector(button3Handler), for: .touchUpInside)
+        /// ]
+        self.keyboardView.button4.addTarget(self, action: #selector(button4Handler), for: .touchUpInside)
+        /// <
+        self.keyboardView.button5.addTarget(self, action: #selector(button5Handler), for: .touchUpInside)
+        /// .
+        self.keyboardView.button6.addTarget(self, action: #selector(button6Handler), for: .touchUpInside)
+        /// ,
+        self.keyboardView.button7.addTarget(self, action: #selector(button7Handler), for: .touchUpInside)
+        /// >
+        self.keyboardView.button8.addTarget(self, action: #selector(button8Handler), for: .touchUpInside)
+        
+        let LTGesture = UILongPressGestureRecognizer(target: self, action: #selector(longDeleteHandler(gesture:)))
+        /// long delete
+        self.keyboardView.delete.addGestureRecognizer(LTGesture)
+        /// delete
+        self.keyboardView.delete.addTarget(self, action: #selector(deleteHandler), for: [.touchDown])
+    }
+    
+    @objc func button1Handler() {
+        
+        let proxy = self.textDocumentProxy
+        proxy.insertText("[")
+    }
+    
+    @objc func button2Handler() {
+
+        let proxy = self.textDocumentProxy
+        proxy.insertText("+")
+    }
+    
+    @objc func button3Handler() {
+        
+        let proxy = self.textDocumentProxy
+        proxy.insertText("-")
+    }
+
+    @objc func button4Handler() {
+        
+        let proxy = self.textDocumentProxy
+        proxy.insertText("]")
+    }
+    
+    @objc func button5Handler() {
+        
+        let proxy = self.textDocumentProxy
+        proxy.insertText("<")
+    }
+    
+    @objc func button6Handler() {
+        
+        let proxy = self.textDocumentProxy
+        proxy.insertText(".")
+    }
+    
+    @objc func button7Handler() {
+        
+        let proxy = self.textDocumentProxy
+        proxy.insertText(",")
+    }
+    
+    @objc func button8Handler() {
+        
+        let proxy = self.textDocumentProxy
+        proxy.insertText(">")
+    }
+    
+    @objc func deleteHandler() {
+        
+        let proxy = self.textDocumentProxy
+        proxy.deleteBackward()
+    }
+
+    @objc func longDeleteHandler(gesture: UILongPressGestureRecognizer) {
+
+        let proxy = self.textDocumentProxy
+
+        if gesture.state == .began {
+            timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+                proxy.deleteBackward()
+            }
+        }
+
+        if gesture.state == .ended {
+            timer?.invalidate()
+        }
     }
     
     override func viewWillLayoutSubviews() {
